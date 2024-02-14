5836582e1b45115143d404da73950aa52a1edc7f
Mia Bongo Norvoll <mia@norvoll.com>
2024-02-14 10:43:12 +0100

    Temp something

--- project/main.go
+++ project/main.go
@@ -20,69 +20,7 @@ type HelloMsg struct {
 	Iter    int
 }
 
-/*
-// func main() {
-// 	// Our id can be anything. Here we pass it on the command line, using
-// 	//  `go run main.go -id=our_id`
-// 	var id string
-// 	flag.StringVar(&id, "id", "", "id of this peer")
-// 	flag.Parse()
 
-// 	// ... or alternatively, we can use the local IP address.
-// 	// (But since we can run multiple programs on the same PC, we also append the
-// 	//  process ID)
-// 	if id == "" {
-// 		localIP, err := localip.LocalIP()
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			localIP = "DISCONNECTED"
-// 		}
-// 		id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
-// 	}
-
-// 	// We make a channel for receiving updates on the id's of the peers that are
-// 	//  alive on the network
-// 	peerUpdateCh := make(chan peers.PeerUpdate)
-// 	// We can disable/enable the transmitter after it has been started.
-// 	// This could be used to signal that we are somehow "unavailable".
-// 	peerTxEnable := make(chan bool)
-// 	go peers.Transmitter(15647, id, peerTxEnable)
-// 	go peers.Receiver(15647, peerUpdateCh)
-
-// 	// We make channels for sending and receiving our custom data types
-// 	helloTx := make(chan HelloMsg)
-// 	helloRx := make(chan HelloMsg)
-// 	// ... and start the transmitter/receiver pair on some port
-// 	// These functions can take any number of channels! It is also possible to
-// 	//  start multiple transmitters/receivers on the same port.
-// 	go bcast.Transmitter(16569, helloTx)
-// 	go bcast.Receiver(16569, helloRx)
-
-// 	// The example message. We just send one of these every second.
-// 	go func() {
-// 		helloMsg := HelloMsg{"Hello from " + id, 0}
-// 		for {
-// 			helloMsg.Iter++
-// 			helloTx <- helloMsg
-// 			time.Sleep(1 * time.Second)
-// 		}
-// 	}()
-
-// 	fmt.Println("Started")
-// 	for {
-// 		select {
-// 		case p := <-peerUpdateCh:
-// 			fmt.Printf("Peer update:\n")
-// 			fmt.Printf("  Peers:    %q\n", p.Peers)
-// 			fmt.Printf("  New:      %q\n", p.New)
-// 			fmt.Printf("  Lost:     %q\n", p.Lost)
-
-// 		case a := <-helloRx:
-// 			fmt.Printf("Received: %#v\n", a)
-// 		}
-// 	}
-// }
-*/
 
 var numFloors int = 4
 
@@ -168,7 +106,7 @@ func (e Elevator) Main() {
     fmt.Printf("%s", e.internal_state)
     for {      
         // Check for stop-button press
-
+        //fmt.Print(e.is_stopped)
         if e.is_stopped {
             fmt.Print("Stop")
             e.Stop()
@@ -177,7 +115,7 @@ func (e Elevator) Main() {
 
         switch state := e.internal_state; state {
         case IDLE:
-            fmt.Printf("Idle")
+            //fmt.Printf("Idle")
             e.pickFloor()
 
         case MOVING:
@@ -213,6 +151,7 @@ func (e Elevator) readChannels(button_order chan elevio.ButtonEvent, current_flo
             e.is_obstructed = io
 
         case is := <-is_stopped:
+            fmt.Printf("Stopping: %t\n", is)
             e.is_stopped = is
         }
     }
@@ -316,7 +255,7 @@ func (e Elevator) visit_floor() {
 func (e Elevator) OpenDoor() {
 
     //Runs only if door is not obstructed
-    obstruction_check := <-e.is_obstructed
+    obstruction_check := e.is_obstructed
 
     if !(obstruction_check){
         elevio.SetDoorOpenLamp(true);
@@ -375,48 +314,6 @@ func (e Elevator) Stop() {
 }
     
 
-    // Handle the current state
-    /*
-    switch e.internal_state {
-    case MOVING:
-        e.internal_state = STOPPED
-        elevio.SetMotorDirection(elevio.MD_Stop)
-        elevio.SetStopLamp(true)
-
-    case AT_FLOOR:
-        e.internal_state = STOPPED
-        elevio.SetMotorDirection(elevio.MD_Stop)
-        elevio.SetStopLamp(true)
-        elevio.SetDoorOpenLamp(true)
-    
-    case DOOR_OPEN:
-        e.internal_state = STOPPED
-        elevio.SetStopLamp(true)
-    }
-    */
-
-    // Reset all order lights
-    //resetLights()
-
-    // Reset timer every time button is pressed
-    /*
-    for {
-        if elevator_stop {
-            e.interrupt_end = time.Now().Add(3*time.Second);
-        }
-        if e.interrupt_end.After(time.Now()) {
-            break
-        }
-    }
-    */
-    
-    // Set state to IDLE
-    /*
-    e.internal_state = IDLE
-    elevio.SetStopLamp(false)
-    elevio.SetDoorOpenLamp(false)
-    */
-
 
 func resetLights() {
     // Reset all order lights
@@ -459,37 +356,5 @@ func main() {
 
     for {}
     
-    // for {
-    //     select {
-    //     case a := <- drv_buttons:
-    //         fmt.Printf("%+v\n", a)
-    //         elevio.SetButtonLamp(a.Button, a.Floor, true)
-            
-    //     case a := <- drv_floors:
-    //         fmt.Printf("%+v\n", a)
-    //         if a == numFloors-1 {
-    //             d = elevio.MD_Down
-    //         } else if a == 0 {
-    //             d = elevio.MD_Up
-    //         }
-    //         elevio.SetMotorDirection(d)
-            
-            
-    //     case a := <- drv_obstr:
-    //         fmt.Printf("%+v\n", a)
-    //         if a {
-    //             elevio.SetMotorDirection(elevio.MD_Stop)
-    //         } else {
-    //             elevio.SetMotorDirection(d)
-    //         }
-            
-    //     case a := <- drv_stop:
-    //         fmt.Printf("%+v\n", a)
-    //         for f := 0; f < numFloors; f++ {
-    //             for b := elevio.ButtonType(0); b < 3; b++ {
-    //                 elevio.SetButtonLamp(b, f, false)
-    //             }
-    //         }
-    //     }
-    // }  
+ 
 }
